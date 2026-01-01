b55745bce06786102d20e04a06d4259ff54161a7
Mateus1502 <mateussamuelcosta@gmail.com>
2026-01-01 19:58:30 -0300

    Velocidade média adicionada

--- Viagem.py
+++ Viagem.py
@@ -4,5 +4,7 @@ consumomedio = int(input("Informe o consumo médio do seu carro:"))
 preco = float(input("Informe o preço do combustível:"))
 litros = distancia / consumomedio #Distância sendo dividida pelos litros por km do carro
 gasto = preco * litros #Preco do litro vezes a quantidade de litros gastos na viagem
+velocidade_media = distancia / tempo
+print("Velocidade média:", round(velocidade_media, 2), "km/h")
 print ("Litros necessários:",round (litros, 2))
 print ("O gasto é de:",round (gasto, 2))
